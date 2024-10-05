41bf81244f9a6269e98ee242682f0271146f484f
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-05 21:58:07 +0530

    add func to login user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -27,6 +28,34 @@ class AuthController extends Controller
         return response()->json([
             'success' => false,
             'message' => 'User not created',
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!$user->email_verified_at) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Email not verified',
+            ]);
+        }
+
+        $credentials = $request->only('email', 'password');
+        $token = Auth::attempt($credentials);
+        $user = Auth::user()->makeHidden('email_verified_at', "created_at", "updated_at", "remember_token");
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'token' => $token,
         ]);
     }
 }
