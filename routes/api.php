bca84d193977d197f0aed81d82b16c6df32f4524
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-05 22:08:30 +0530

    add default route to check api is up or not

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+Route::get("/api",function(){
+    return response()->json([
+        "status"=>"success",
+        "message"=>"Welcome to SnapAura API",
+        "date" => now()
+    ]);
+});
+
 Route::post("register", [AuthController::class, "store"]);
 Route::post("reset", [AuthController::class, "passwordReset"]);
 Route::post("login", [AuthController::class, "login"]);
