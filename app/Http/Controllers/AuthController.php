9e4d70609f69a17a057c1712acc025b37681f614
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-05 21:56:59 +0530

    add func to store user info

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $data = [
+            "email" => $request->email,
+            "password" => $request->password,
+            "name" => $request->name
+        ];
+        $register = User::create($data);
+
+        if ($register) {
+            // $this->sendEmail($isCreate);
+            return response()->json([
+                'success' => true,
+                'message' => 'User created successfully',
+                'data' => $register
+            ]);
+        }
+        return response()->json([
+            'success' => false,
+            'message' => 'User not created',
+        ]);
+    }
 }
