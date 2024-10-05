b9113b7ed86824a47732b5b3c99b47d380151f5d
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-05 21:49:56 +0530

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,20 +6,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    use HasFactory, Notifiable, HasUuids;
     protected $fillable = [
         'name',
         'email',
         'password',
+        'email_verified_at',
+        "remember_token",
     ];
 
     /**
@@ -40,8 +37,10 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            'email_verified_at' => 'datetime:Y-m-d',
             'password' => 'hashed',
+            'created_at' => 'datetime:Y-m-d',
+            'updated_at' => 'datetime:Y-m-d',
         ];
     }
 }
