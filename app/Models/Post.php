27eccb1588e3dca2c49ceb556b0d0869897dc0b9
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-05 23:23:37 +0530

    add fillable in post model class

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        "image",
+        "caption"
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime:Y-m-d',
+            'updated_at' => 'datetime:Y-m-d',
+            "user_id" => auth()->user()->id
+        ];
+    }
 }
