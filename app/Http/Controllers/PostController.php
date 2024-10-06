a9e540a9d32740177b2de0aa87998099403ea97e
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-06 11:02:59 +0530

    write func to store Image in db

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
+use Storage;
 
 class PostController extends Controller
 {
-    //
+    public function store(PostRequest $request)
+    {
+        $data = [
+            "image" => $request->image,
+            "title" => $request->title,
+        ];
+    }
+
+    protected function uploadImage($file)
+    {
+        $uploadFolder = 'posts';
+        $image = $file;
+        $image_uploaded_path = $image->store($uploadFolder, 'public');
+        $uploadedImageUrl = Storage::disk('public')->url($image_uploaded_path);
+
+        return $uploadedImageUrl;
+    }
 }
