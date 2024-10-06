b9e73773b143219472e19faaccb964779678cf63
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-06 18:50:37 +0530

    write migration file for pool table

--- database/migrations/2024_10_06_131836_create_pools_table.php
+++ database/migrations/2024_10_06_131836_create_pools_table.php
@@ -4,15 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('pools', function (Blueprint $table) {
-            $table->id();
+            $table->uuid("id")->primary();
+            $table->string("question");
+            $table->json("options");
+            $table->foreignUuid("created_by")->constrained("users")->onDelete("cascade");
             $table->timestamps();
         });
     }
