7776e468331bb1104958d42cbefa396bd5eae089
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-06 19:05:14 +0530

    write model class for poll vote

--- app/Models/PoolVote.php
+++ app/Models/PoolVote.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class PoolVote extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->user_id = auth()->user()->id;
+        });
+    }
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime:Y-m-d',
+            'updated_at' => 'datetime:Y-m-d',
+        ];
+    }
 }
