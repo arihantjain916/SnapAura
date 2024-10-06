08f9f9f33edcd40aae121daed0ae96fcd1618f08
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-06 14:30:19 +0530

    add func to display all comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Transformers\CommentTransform;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function display()
+    {
+        $comments = Comment::with('replies', 'user')
+            ->whereNull('parent_id')
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+
+        $nextPageUrl = $comments->nextPageUrl();
+        $previousPageUrl = $comments->previousPageUrl();
+        $count = $comments->count();
+
+        $comments = fractal([$comments], new CommentTransform())->toArray();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $comments["data"][0][0],
+            "meta" => [
+                "next_page_url" => $nextPageUrl,
+                "previous_page_url" => $previousPageUrl,
+                "total_comment" => $count
+            ]
+        ], 200);
+    }
 }
