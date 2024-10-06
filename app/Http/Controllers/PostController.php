647fea226938c13d8caab863829fa31338559134
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-06 11:07:48 +0530

    write func to store data in db

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,16 +3,45 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\PostRequest;
+use App\Models\Post;
 use Storage;
+use DB;
 
 class PostController extends Controller
 {
     public function store(PostRequest $request)
     {
-        $data = [
-            "image" => $request->image,
-            "title" => $request->title,
-        ];
+        try {
+            $data = [
+                "image" => $this->uploadImage($request->file('image')),
+                "title" => $request->title,
+            ];
+
+            DB::beginTransaction();
+
+            $post = Post::create($data);
+
+            DB::commit();
+
+            if (!$post) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Post not created',
+                ]);
+            }
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Post created successfully',
+                'data' => $post
+            ]);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                "success" => false,
+                "error" => $e->getMessage()
+            ]);
+        }
     }
 
     protected function uploadImage($file)
