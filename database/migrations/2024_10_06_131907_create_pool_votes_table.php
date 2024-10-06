f0cc54c8443be8762d44a998c7c80c9826ac7d32
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-06 18:59:09 +0530

    write migration file for pool vote table

--- database/migrations/2024_10_06_131907_create_pool_votes_table.php
+++ database/migrations/2024_10_06_131907_create_pool_votes_table.php
@@ -4,15 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('pool_votes', function (Blueprint $table) {
-            $table->id();
+            $table->uuid("id")->primary();
+            $table->foreignUuid("pool");
+            $table->foreignUuid("user");
+            $table->string("option");
             $table->timestamps();
         });
     }
