3f08cd6627cedeb655d2f706a819f9aad778a7d5
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-06 14:25:20 +0530

    add relation in comment field

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,8 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Comment extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        "user_id",
+        "comment",
+        "parent_id",
+        "post"
+    ];
+
+    public function replies()
+    {
+        return $this->hasMany(Comment::class, 'parent_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function casts()
+    {
+        return [
+            "created_at" => "datetime:d-m-Y",
+            "updated_at" => "datetime:d-m-Y",
+        ];
+    }
 }
