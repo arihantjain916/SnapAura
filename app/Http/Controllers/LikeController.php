24ec7575cf59de4cee18713ce234be3cb839a15e
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-13 13:03:22 +0530

    feat: add controller to handle Like

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateLikeRequest;
+use App\Models\PostLike;
+use DB;
+use Illuminate\Http\Request;
+
+class LikeController extends Controller
+{
+    public function like($post_id)
+    {
+        try {
+            $data = [
+                "user_id" => auth()->user()->id,
+                "post_id" => $post_id
+            ];
+
+            DB::beginTransaction();
+            $like = PostLike::create($data);
+
+            DB::commit();
+            return response()->json([
+                "message" => "Post Liked Successfully",
+                "like" => $like,
+                "success" => true
+            ], 200);
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                "success" => false,
+                "message" => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function unlike($post_id)
+    {
+        try {
+            $data = [
+                "user_id" => auth()->user()->id,
+                "post_id" => $post_id
+            ];
+
+            DB::beginTransaction();
+            PostLike::where("user_id", $data["user_id"])->where("post_id", $data["post_id"])->delete();
+
+            DB::commit();
+            return response()->json([
+                "message" => "Post UnLiked Successfully",
+                "success" => true
+            ], 200);
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                "success" => false,
+                "message" => $e->getMessage()
+            ], 500);
+        }
+    }
+}
