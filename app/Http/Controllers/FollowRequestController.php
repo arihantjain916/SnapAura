fa9842e4ff3ac5ab97a9ff9a6fe35adaddaf4ce5
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-21 09:52:18 +0530

    feat: add follow request controller to handle request

---
+++ app/Http/Controllers/FollowRequestController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Follow;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class FollowRequestController extends Controller
+{
+    public function send($id)
+    {
+        $user_id = auth()->user()->id;
+        $follower_id = $id;
+
+        $isIdExist = User::find($id);
+
+        if (!$isIdExist) {
+            return response()->json([
+                "status" => "error",
+                "message" => "User not found"
+            ], 404);
+        }
+
+        if ($user_id == $follower_id) {
+            return response()->json([
+                "status" => "error",
+                "message" => "You can't follow yourself"
+            ], 404);
+        }
+        $existingRequest = Follow::where('follower_id', $user_id)
+            ->where('followed_id', $follower_id)
+            ->first();
+
+        if ($existingRequest) {
+            return response()->json(['status' => false, 'message' => 'Follow request already sent'], 400);
+        }
+
+        Follow::create([
+            'follower_id' => $user_id,
+            'followed_id' => $follower_id,
+            'status' => 'pending',
+        ]);
+
+        return response()->json(['status' => true, 'message' => 'Follow request sent successfully'], 200);
+    }
+}
