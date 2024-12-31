24776befef4f84c7fe9aa9cd4907079bec95de13
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-31 10:52:41 +0530

    feat: optimize notification controller

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -11,23 +11,10 @@ class NotificationController extends Controller
 {
     public function fetch()
     {
-        $notifications = auth()->user()->notifications()->get();
+        $notifications = Notification::with(["user", "meta.post","meta.user"])->where("user_id", auth()->user()->id)->get();
         return response()->json([
             "status" => true,
             "data" => $notifications ?? []
         ], 200);
-    }
-
-
-    public function temp()
-    {
-        $notification = Notification::first();
-        $user = User::first();
-        $second = User::where("username", "arihant_getgrahak")->first();
-        event(new NotificationEvent($notification, $user, $second));
-
-        return response()->json([
-            "status" => true
-        ]);
     }
 }
